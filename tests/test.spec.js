75e663864ff9062fe4c34cf015da72a6412d971f
kunalSinghQA <kunal.s@feuji.com>
2023-04-09 00:04:03 +0530

    updating demo scripts

---
+++ tests/test.spec.js
@@ -0,0 +1,11 @@
+import {test, expect} from "@playwright/test";
+
+test("TC-01 Testing navigation", async( { page } ) => {
+    page.goto('https://www.shoppersstop.com/');
+    await expect(page.locator('[title="SHOPPERS STOP"].desk-logo')).toBeVisible();
+    await expect(page.locator('a[title="MEN"]')).toBeVisible();
+    await page.locator('a[title="MEN"]').click();
+    await expect(page).toHaveURL(/men/);
+    await expect(page.locator('.product-tile a.productdetail').first()).toBeVisible();
+    await page.locator('.product-tile a.productdetail').first().click();
+})
